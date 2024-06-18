6c742251327a252c6e445ba8e371186fa9d0245d
João Filho <joaofilhomil@gmail.com>
2024-06-18 15:58:10 -0300

    :package: build: Adicionar função para carregar dados

--- clinica.js
+++ clinica.js
@@ -5,3 +5,16 @@ const DATA_FILE = 'dados.json';
 
 let pacientes = [];
 let consultas = [];
+
+function carregarDados() {
+    try {
+      const data = fs.readFileSync(DATA_FILE, 'utf8');
+      const parsedData = JSON.parse(data);
+      pacientes = parsedData.pacientes || [];
+      consultas = parsedData.consultas || [];
+      console.log('Dados carregados com sucesso.');
+    } catch (err) {
+      console.log('Nenhum dado salvo encontrado. Iniciando com dados vazios.');
+    }
+  }
+  
